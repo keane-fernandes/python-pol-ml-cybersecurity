3d066c0bd822f69b18cfd01bc90e8a96587f1646
keane-fernandes <kf13056@my.bristol.ac.uk>
2021-08-05 20:16:44 +0100

    added packet summaries

--- capture_utilities.py
+++ capture_utilities.py
@@ -1,14 +1,29 @@
 import pyshark
 import collections
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
+# Import packets from capture file
 capture = pyshark.FileCapture("./pcaps/baseline-1.pcapng", only_summaries=True)
+protocolList = []
+numberOfPackets = 0
+# Iterate though packets and create lists of each attribute
+for packet in capture:
+    numberOfPackets += 1
+    line = str(packet)
+    formattedLine = line.split(" ")
+    protocolList.append(formattedLine[4])
 
-print(capture[0])
+counter = collections.Counter(protocolList)
 
+# Plot our results
+plt.style.use("ggplot")
+y_pos = np.arange(len(list(counter.keys())))
+plt.bar(
+    y_pos, list(counter.values()), align="center", alpha=0.5, color=["b", "g", "r", "c"]
+)
+plt.xticks(y_pos, list(counter.keys()))
+plt.ylabel("Frequency")
+plt.show()
 
-if False:
-    print("Packet Summary: \n")
-    print(example_packet)
-    print("Packet Length: " + example_packet.length + "\n")
+print(numberOfPackets)
