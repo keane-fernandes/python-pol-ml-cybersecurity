d1d5b31f6750b8cc9142a2371e14c1bfadb8c1d0
keane-fernandes <kf13056@my.bristol.ac.uk>
2021-08-09 10:06:46 +0100

    changed file structure

---
+++ live.py
@@ -0,0 +1,6 @@
+import pyshark
+import tshark
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+
---
+++ static.py
@@ -0,0 +1,70 @@
+import pyshark
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import packets from capture file (Change this to accept command
+# line arguments, basically make it more flexible)
+
+
+def is_not_blank(s):
+    return bool(s and not s.isspace())
+
+
+capture = pyshark.FileCapture("./pcaps/baseline-1.pcapng", only_summaries=False)
+
+# Create attribute lists
+indices = []
+timestamps = []
+source = []
+destination = []
+protocolList = []
+packetLength = []
+throttlePedalDemand = []
+brakeStatus = []
+
+numberOfPackets = 0
+
+# Iterate though packets and populate attribute lists
+for packet in capture:
+    numberOfPackets += 1
+    line = str(packet)
+    formattedLine = line.split(" ")
+    indices.append(formattedLine[0])
+    timestamps.append(formattedLine[1])
+    source.append(formattedLine[2])
+    destination.append(formattedLine[3])
+    protocolList.append(formattedLine[4])
+    packetLength.append(formattedLine[5])
+    throttlePedalDemand.append(formattedLine[6])
+    brakeStatus.append(formattedLine[7])
+
+counter = collections.Counter(protocolList)
+
+# Real-time sniffing of packets
+# Specify interface to sniff on
+
+
+# Plot our results
+if False:
+    plt.style.use("ggplot")
+    y_pos = np.arange(len(list(counter.keys())))
+    plt.bar(
+        y_pos,
+        list(counter.values()),
+        align="center",
+        alpha=0.5,
+        color=["b", "g", "r", "c"],
+    )
+    plt.xticks(y_pos, list(counter.keys()))
+    plt.ylabel("Frequency")
+    plt.show()
+
+print("Number of packets processed: " + str(numberOfPackets))
+print("Size of formatted line list: " + str(len(formattedLine)))
+print("Number of timestamps processed: " + str(len(timestamps)))
+print("Number of sources processed: " + str(len(source)))
+print("Number of destinations processed: " + str(len(destination)))
+print("Number of protocols processed: " + str(len(protocolList)))
+print("Number of lengths processed: " + str(len(packetLength)))
+print("These are all of the elements in the formatted line: \n")
