1bfd9ad6639797e0b968ba27536bc469f65b3f9e
keane-fernandes <kf13056@my.bristol.ac.uk>
2021-08-18 11:17:58 +0100

    base parser working

--- pol_utilities.py
+++ pol_utilities.py
@@ -1,29 +1,59 @@
-# Contains helper methods, class definitions and global constants to
-# aid pattern of life implementation
+# Contains helper methods, class definitions and constants to
+# aid Pattern of Life (PoL) framework implementation
 
-# There are six types of status:
+# There are six types of statuses:
 #   0 - Broadcast
 #   1 - Vehicle Speed
 #   2 - Throttle Pedal
 #   3 - Brake Status
 #   4 - Cruise Control
-#   5 - Unknown
+#   5 - Unknown / Malicious
 
 # Libraries
 import pyshark.packet as p
 
-# Hexadecimal constants that identify the type of packet
-__BRAKESTATUS_SID = "aa00abc2"
-__BRAKESTATUS_IID = "5500574f"
+# Constants
 
-__THROTTLEPEDALSTATUS_SID = "aa00aba1"
-__THROTTLEPEDALSTATUS_IID = "5500572e"
+# General
+__DEFAULT_PACKET_LENGTH = 122
+__DEFAULT_UDP_LENGTH = 88
+__DEFAULT_PACKET_LAYERS = 4
+__DEFAULT_BROADCAST_LAYERS = 2
+__DEFAULT_SID_START_BYTE = 52
+__DEFAULT_SID_END_BYTE = 56
+__DEFAULT_IID_START_BYTE = 56
+__DEFAULT_IID_END_BYTE = 60
 
-__VEHICLESPEEDSTATUS_SID = "aa00aba4"
-__VEHICLESPEEDSTATUS_IID = "55005731"
+# Broadcast Packet
+__BROADCAST_STATUS_ID = 0
+__BROADCAST_LENGTH = 60
+__BROADCAST_SID = "ffffffff"
+__BROADCAST_IID = "ffffffff"
 
-__CRUISECONTROLSTATUS_SID = "aa00ab9b"
-__CRUISECONTROLSTATUS_IID = "55005728"
+# Vehicle Speed Status Packet (Packet and UDP lengths are different from default)
+__VEHICLESPEEDSTATUS_STATUS_ID = 1
+__VEHICLESPEEDSTATUS_LENGTH = 130
+__VEHICLESPEEDSTATUS_UDP_LENGTH = 96
+__VEHICLESPEEDSTATUS_SID = "aa00aba4"  # Hexadecimal
+__VEHICLESPEEDSTATUS_IID = "55005731"  # Hexadecimal
+
+# Throttle Status Packet
+__THROTTLESTATUS_STATUS_ID = 2
+__THROTTLEPEDALSTATUS_SID = "aa00aba1"  # Hexadecimal
+__THROTTLEPEDALSTATUS_IID = "5500572e"  # Hexadecimal
+
+# Brake Status Packet
+__BRAKESTATUS_STATUS_ID = 3
+__BRAKESTATUS_SID = "aa00abc2"  # Hexadecimal
+__BRAKESTATUS_IID = "5500574f"  # Hexadecimal
+
+# Cruise Control Status Packet
+__CRUISECONTROL_STATUS_ID = 4
+__CRUISECONTROLSTATUS_SID = "aa00ab9b"  # Hexadecimal
+__CRUISECONTROLSTATUS_IID = "55005728"  # Hexadecimal
+
+# Malicious Packet
+__MALICIOUSPACKET_STATUS = 5
 
 # Checks that layers exist in a packet
 def check_for_layers(packet, *layers):
@@ -43,6 +73,7 @@ def check_number_of_layers(packet, number):
     return True
 
 
+# Checks that all attributes exist in a layer
 def check_attributes_in_layer(layer, *attributes):
     for attribute in attributes:
         if not hasattr(layer, attribute):
@@ -51,18 +82,21 @@ def check_attributes_in_layer(layer, *attributes):
     return True
 
 
-# Defines a status ID depending on the type of packet sent
-def compute_status_type(packet):
-    packet_length = packet.frame_info.len
+# Defines a status type (0 - 5) depending on the sid, iid
+def compute_status_type(sid, iid):
+    if sid == __VEHICLESPEEDSTATUS_SID and iid == __VEHICLESPEEDSTATUS_IID:
+        return __VEHICLESPEEDSTATUS_STATUS_ID
 
-    if packet_length == 60:
-        return 0
+    if sid == __THROTTLEPEDALSTATUS_SID and iid == __THROTTLEPEDALSTATUS_IID:
+        return __THROTTLESTATUS_STATUS_ID
 
-    if packet_length == 130:
-        return 1
+    if sid == __BRAKESTATUS_SID and iid == __BRAKESTATUS_IID:
+        return __BRAKESTATUS_STATUS_ID
 
-    if packet_length == 122:
-        return 2
+    if sid == __CRUISECONTROLSTATUS_SID and iid == __CRUISECONTROLSTATUS_IID:
+        return __CRUISECONTROL_STATUS_ID
+
+    return __MALICIOUSPACKET_STATUS
 
 
 # Calculates the throughput of a network in packets/second
@@ -77,40 +111,76 @@ def calculate_packet_throughput(packets, time):
 # Checks if a packet is valid (i.e. contains information on vehicle speed
 # status, cruise control, brake status or throttle status)
 def check_for_valid(packet):
-    if not check_number_of_layers(packet, 4):
+
+    # Check packet layers
+    if not check_number_of_layers(packet, __DEFAULT_PACKET_LAYERS):
         return False
     if not check_for_layers(packet, "eth", "ip", "udp", "data"):
         return False
 
-    packet_length = packet.frame_info.len
-    if packet_length != 130 or packet_length != 122:
+    # Check layer attributes [IMPORTANT]
+
+    # Check packet and udp payload sizes
+    packet_length = int(packet.frame_info.len)
+    udp_payload_length = int(packet.udp.length)
+
+    if (
+        packet_length != __VEHICLESPEEDSTATUS_LENGTH
+        and packet_length != __DEFAULT_PACKET_LENGTH
+    ):
+        return False
+
+    if (
+        udp_payload_length != __DEFAULT_UDP_LENGTH
+        and udp_payload_length != __VEHICLESPEEDSTATUS_UDP_LENGTH
+    ):
         return False
 
     return True
 
 
-def check_for_vehicle_status(packet):
-    pass
+# Returns a concatenate string of bytes between start_byte (inclusive) and end_byte (not inclusive)
+def extract_bytes(byte_field, start_byte, end_byte):
+    return "".join(byte_field[start_byte:end_byte])
 
 
-def check_for_other(packet):
-    pass
+# Returns SID from the Service Message Header
+def extract_sid(byte_field):
+    return extract_bytes(byte_field, __DEFAULT_SID_START_BYTE, __DEFAULT_SID_END_BYTE)
+
+
+# Returns IID from the Service Message Header
+def extract_iid(byte_field):
+    return extract_bytes(byte_field, __DEFAULT_IID_START_BYTE, __DEFAULT_IID_END_BYTE)
+
+
+def get_broadcast_sid():
+    return __BROADCAST_SID
+
+
+def get_broadcast_iid():
+    return __BROADCAST_SID
 
 
 # Checks if a packet matches a broadcast message sent by the input controller
 def check_for_broadcast(packet):
-    if not check_number_of_layers(packet, 2):
+    if not check_number_of_layers(packet, __DEFAULT_BROADCAST_LAYERS):
         return False
 
     if not check_for_layers(packet, "eth", "data"):
         return False
 
-    packet_length = packet.frame_info.len
+    packet_length = int(packet.frame_info.len)
 
-    if packet_length != 60:
+    if packet_length != __BROADCAST_LENGTH:
         return False
 
     return True
+
+
+# ====================================================================
+# Helper Methods
+# ====================================================================
 
 
 # Extract relevant information
--- static.py
+++ static.py
@@ -21,7 +21,7 @@ time = 0.0
 
 # Import packets from capture file
 capture = pyshark.FileCapture(
-    "./pcaps/test.pcapng", only_summaries=False, keep_packets=False
+    "./pcaps/baseline-1.pcapng", only_summaries=False, keep_packets=False
 )
 
 # Iterate though packets and populate PolPacket object
@@ -29,8 +29,16 @@ for packet in capture:
     packet_length = packet.frame_info.len
 
     if pu.check_for_valid(packet):
-        hexdump = packet.data.data
-        status_type = pu.compute_status_type(packet)
+
+        byte_field = packet.udp.payload.split(":")
+
+        sid = pu.extract_sid(byte_field)
+        iid = pu.extract_iid(byte_field)
+
+        status_type = pu.compute_status_type(sid, iid)
+
+        print(status_type)
+
         time_relative = packet.udp.time_relative
         time_delta = packet.udp.time_delta
         total_length = packet_length
@@ -39,9 +47,7 @@ for packet in capture:
         destination_port = packet.udp.dstport
         source_ip = packet.ip.src
         destination_ip = packet.ip.dst
-        keane = packet.data.ip
-        sid = "1234556"
-        iid = "1234060"
+        hexdump = packet.data.data
 
         # IP layer data extraction
 
@@ -65,14 +71,18 @@ for packet in capture:
         pu.PolPacket.increment_packet_counter()
 
     elif pu.check_for_broadcast(packet):
+        sid = pu.get_broadcast_sid()
+        iid = pu.get_broadcast_iid()
+
+        status_type = pu.compute_status_type(sid, iid)
+
         pu.PolPacket.increment_packet_counter()
-        pass
+        print(status_type)
     else:
+
         pu.PolPacket.increment_packet_counter()
-        pass
 
 print("Total Packets: " + str(pu.PolPacket.counter))
-print(hasattr(packet.udp, "payload"))
 
 # Plot our results
 if False:
