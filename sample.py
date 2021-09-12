c2c5ee5446a5f51109517b557d62e9fdfb279ad9
keane-fernandes <kf13056@my.bristol.ac.uk>
2021-09-12 17:46:26 +0100

    added some more pcaps for testing

--- sample.py
+++ sample.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import pyshark as ps
 import pol_utilities as pu
+
+
+string = "ab:cd:ef"
+split = string.split(":")
+
+print(split)
--- testing.py
+++ testing.py
@@ -15,15 +15,23 @@ test_directory = os.path.join(cwd, pu.root.get("testing"))
 
 class UtilityChecks(unittest.TestCase):
     def test_extract_bytes(self):
-        test_byte_field = "a1:21:23"
-        output = pu.extract_bytes(test_byte_field, 0, 1)
-        print("\n")
-        print(output)
+        test_byte_field = ["ab", "cd", "ef"]
+        self.assertEqual(pu.extract_bytes(test_byte_field, 0, 0), "")
+        self.assertEqual(pu.extract_bytes(test_byte_field, 0, 1), "ab")
+        self.assertEqual(pu.extract_bytes(test_byte_field, 0, 2), "abcd")
+        self.assertEqual(pu.extract_bytes(test_byte_field, 100, 93), "")
+        self.assertEqual(pu.extract_bytes(test_byte_field, 0, 0), "")
+
+        test_byte_field = []
+        self.assertEqual(pu.extract_bytes(test_byte_field, 0, 0), "")
+        self.assertEqual(pu.extract_bytes(test_byte_field, 0, 1), "")
+        self.assertEqual(pu.extract_bytes(test_byte_field, 0, 2), "")
+        self.assertEqual(pu.extract_bytes(test_byte_field, 100, 93), "")
+        self.assertEqual(pu.extract_bytes(test_byte_field, 0, 0), "")
 
 
 class PacketChecks(unittest.TestCase):
     def test_speed(self):
-        # Check number of layers in all kinds of packets
         file_path = os.path.join(test_directory, "valid_speed.pcapng")
         capture = ps.FileCapture(file_path, only_summaries=False)
         packet = capture[0]
@@ -70,6 +78,8 @@ class PacketChecks(unittest.TestCase):
         file_path = os.path.join(test_directory, "dhcp.pcapng")
         capture = ps.FileCapture(file_path, only_summaries=False)
         packet = capture[0]
+        self.assertTrue(pu.check_number_of_layers(packet, 4))
+        self.assertTrue(pu.check_for_layers(packet, "dhcp"))
         capture.close()
 
     def test_mdns(self):
@@ -103,8 +113,109 @@ class DissectorChecks(unittest.TestCase):
         file_path = os.path.join(test_directory, "valid_speed.pcapng")
         capture = ps.FileCapture(file_path, only_summaries=False)
         packet = capture[0]
+
         byte_field = packet.udp.payload.split(":")
-        self.assertEqual(0, 0)
+
+        sid = pu.extract_sid(byte_field)
+        iid = pu.extract_iid(byte_field)
+
+        self.assertEqual(sid, "aa00aba4")
+        self.assertEqual(iid, "55005731")
+
+        status_type = pu.compute_status_type(sid, iid)
+
+        self.assertEqual(status_type, "SPEED")
+        self.assertEqual(pu.extract_payload(byte_field, status_type), 10)
+
+    def test_throttle_dissection(self):
+        file_path = os.path.join(test_directory, "valid_throttle.pcapng")
+        capture = ps.FileCapture(file_path, only_summaries=False)
+        packet = capture[0]
+
+        byte_field = packet.udp.payload.split(":")
+
+        sid = pu.extract_sid(byte_field)
+        iid = pu.extract_iid(byte_field)
+
+        self.assertEqual(sid, "aa00aba1")
+        self.assertEqual(iid, "5500572e")
+
+        status_type = pu.compute_status_type(sid, iid)
+
+        self.assertEqual(status_type, "THROTTLE")
+        self.assertEqual(pu.extract_payload(byte_field, status_type), 10)
+
+    def test_speed_dissection(self):
+        file_path = os.path.join(test_directory, "valid_speed.pcapng")
+        capture = ps.FileCapture(file_path, only_summaries=False)
+        packet = capture[0]
+
+        byte_field = packet.udp.payload.split(":")
+
+        sid = pu.extract_sid(byte_field)
+        iid = pu.extract_iid(byte_field)
+
+        self.assertEqual(sid, "aa00aba4")
+        self.assertEqual(iid, "55005731")
+
+        status_type = pu.compute_status_type(sid, iid)
+
+        self.assertEqual(status_type, "SPEED")
+        self.assertEqual(pu.extract_payload(byte_field, status_type), 10)
+
+    def test_throttle_dissection(self):
+        file_path = os.path.join(test_directory, "valid_throttle.pcapng")
+        capture = ps.FileCapture(file_path, only_summaries=False)
+        packet = capture[0]
+
+        byte_field = packet.udp.payload.split(":")
+
+        sid = pu.extract_sid(byte_field)
+        iid = pu.extract_iid(byte_field)
+
+        self.assertEqual(sid, "aa00aba1")
+        self.assertEqual(iid, "5500572e")
+
+        status_type = pu.compute_status_type(sid, iid)
+
+        self.assertEqual(status_type, "THROTTLE")
+        self.assertEqual(pu.extract_payload(byte_field, status_type), 486)
+
+    def test_brake_dissection(self):
+        file_path = os.path.join(test_directory, "valid_brake.pcapng")
+        capture = ps.FileCapture(file_path, only_summaries=False)
+        packet = capture[0]
+
+        byte_field = packet.udp.payload.split(":")
+
+        sid = pu.extract_sid(byte_field)
+        iid = pu.extract_iid(byte_field)
+
+        self.assertEqual(sid, "aa00abc2")
+        self.assertEqual(iid, "5500574f")
+
+        status_type = pu.compute_status_type(sid, iid)
+
+        self.assertEqual(status_type, "BRAKE")
+        self.assertEqual(pu.extract_payload(byte_field, status_type), 0)
+
+    def test_cruise_dissection(self):
+        file_path = os.path.join(test_directory, "valid_cruise.pcapng")
+        capture = ps.FileCapture(file_path, only_summaries=False)
+        packet = capture[0]
+
+        byte_field = packet.udp.payload.split(":")
+
+        sid = pu.extract_sid(byte_field)
+        iid = pu.extract_iid(byte_field)
+
+        self.assertEqual(sid, "aa00ab9b")
+        self.assertEqual(iid, "55005728")
+
+        status_type = pu.compute_status_type(sid, iid)
+
+        self.assertEqual(status_type, "CRUISE")
+        self.assertEqual(pu.extract_payload(byte_field, status_type), 0)
 
 
 if __name__ == "__main__":
