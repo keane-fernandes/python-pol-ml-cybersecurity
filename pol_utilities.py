743f736e7635f3401cf40e0b61200818c572b953
Keane Fernandes <31629150+keane-fernandes@users.noreply.github.com>
2021-10-06 09:12:37 +0100

    Update pol_utilities.py

--- pol_utilities.py
+++ pol_utilities.py
@@ -4,13 +4,6 @@ Hardware:       Automotive Rig v1.0 from Thales
 Description:    This is a python utility that contains helper methods and constants
                 to aid 01_collect.py and 02_feature.py.
 
-                There are six types of statuses:
-                    B - Broadcast
-                    SPEED - Vehicle Speed
-                    THROTTLE - Throttle Pedal
-                    BRAKE - Brake Status
-                    CRUISE - Cruise Control
-                    M - Unknown / Malicious
 
 User Iput:      None required.
 Usage:          It is a helper file, cannot be opened directly.
